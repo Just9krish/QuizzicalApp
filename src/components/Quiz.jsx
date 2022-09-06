da707bb6422ce4314b8d81d03ab098ecbcc76dc4
just9krish <just9krish434@gamil.com>
2022-09-06 15:11:32 +0530

    set basic styling

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -20,21 +20,26 @@ export default function Quiz() {
     // console.log(allQuiz)
 
     const questionsElements = allQuiz.map(quiz => (
-        <div className="text-blue-900">
-            <p className="text-base font-bold">{quiz.question}</p>
+        <div className="text-blue-900 space-y-5">
+            <p className="text-lg font-bold">{quiz.question}</p>
             <div className="flex space-x-5">
-                <button className="border-none border-2 border-blue-700 rounded-md">{quiz.correct_answer}</button>
-                <button className="border-none border-2 border-blue-700 rounded-md">{quiz.incorrect_answers[0]}</button>
-                <button className="border-none border-2 border-blue-700 rounded-md">{quiz.incorrect_answers[1]}</button>
-                <button className="border-none border-2 border-blue-700 rounded-md">{quiz.incorrect_answers[2]}</button>
+                <button className="border border-blue-700 rounded-md py-1 px-6 font-medium hover:bg-slate-300 hover:border hover:border-slate-300">{quiz.correct_answer}</button>
+                <button className="border border-blue-700 rounded-md py-1 px-6 font-medium hover:bg-slate-300 hover:border hover:border-slate-300">{quiz.incorrect_answers[0]}</button>
+                <button className="border border-blue-700 rounded-md py-1 px-6 font-medium hover:bg-slate-300 hover:border hover:border-slate-300">{quiz.incorrect_answers[1]}</button>
+                <button className="border border-blue-700 rounded-md py-1 px-6 font-medium hover:bg-slate-300 hover:border hover:border-slate-300">{quiz.incorrect_answers[2]}</button>
             </div>
+            <hr className="bg-slate-500"></hr>
         </div>
     ))
 
     return (
-        <div className="space-y-5">
+        <div className="space-y-5 max-w-5xl mx-auto my-11 p-5">
             {/* {JSON.stringify(allQuiz, null, 2)} */}
             {questionsElements}
+            <div className="flex justify-center items-center">
+                <p className="font-bold text-lg text-blue-900 mr-4">You scored 4/5 correct answers</p>
+                <button className="bg-blue-900 text-white px-6 py-2 rounded-lg font-semibold hover:bg-blue-700">Check answers</button>
+            </div>
         </div>
     )
 }
