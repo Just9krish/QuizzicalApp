03f8a3cc0c2cabfb4962bb468b2b9999dcd89305
just9krish <just9krish434@gamil.com>
2022-09-09 15:28:03 +0530

    made array object to hold the data which fetch by api

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,7 @@ import Quiz from './components/Quiz'
 function App() {
   const [isStart, setIsStart] = useState(false)
   const [allQuiz, setAllQuiz] = useState([])
-  const [quiz, setQuiz] = useState({
-    question: '',
-    answer: '',
-    options: '',
-    id: nanoid()
-  })
+
 
   function startGame() {
     setIsStart(prevValue => !prevValue)
@@ -22,36 +17,63 @@ function App() {
   useEffect(() => {
     fetch("https://opentdb.com/api.php?amount=5&category=18&type=multiple")
       .then(response => response.json())
-      .then(data => data.results)
-      .then(quizQuestions => setAllQuiz(quizQuestions))
+      .then(data => setAllQuiz(newQuiz(data.results)))
   }, [0])
 
-  // console.log(allQuiz)
-
-  function getQuiz() {
-    setQuiz(prevQuiz => {
-      return {
-        ...prevQuiz,
-        question : allQuiz
-      }
-    })
+  function newQuiz(listOfQuizs) {
+    return listOfQuizs.map(quiz => ({
+      question: quiz.question,
+      correctAnswer: quiz.correct_answer,
+      // options: settingOptions(shuffleArray([...quiz.incorrect_answers, quiz.correct_answer]))
+    }))
   }
 
-  function options() {
-    
+  function shuffleArray(arr) {
+    for (let i = arr.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * (i + 1));
+      [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+    return arr;
   }
+
+  function settingOptions(listOfOptions) {
+    return listOfOptions.map(option => ({
+      id: nanoid(),
+    }))
+  }
+
+  const quizElements = allQuiz.map(quiz => (
+    <Quiz
+      question={quiz.question}
+      key={quiz.id}
+      id={quiz.id}
+    />
+  ))
 
   return (
+    // <main>
+    //   {
+    //     !isStart ?
+    //       <Start
+    //         startGame={startGame}
+    //       /> :
+    //       { quizElements }
+    //   }
+    // </main>
     <main>
-      {
-        !isStart ?
-          <Start
-            startGame={startGame}
-          /> :
-          <Quiz
-            startGame={startGame}
-            allQuiz={allQuiz}
-          />
+      {!isStart ?
+        <Start
+          startGame={startGame}
+        /> :
+        <div className="space-y-5 max-w-5xl mx-auto my-11 p-5">
+          {quizElements}
+          <div className="flex justify-center items-center">
+            <p className="font-bold text-lg text-clr-blue-text mr-4">You scored 4/5 correct answers</p>
+            <button
+              onClick={startGame}
+              className="bg-clr-blue-btn text-clr-white px-6 py-2 rounded-lg font-semibold hover:bg-blue-700">Check answers</button>
+          </div>
+        </div>
       }
     </main>
   )
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,57 +1,15 @@
 export default function Quiz(props) {
 
-    function shuffleArray(arr) {
-        for (let i = arr.length - 1; i > 0; i--) {
-            let j = Math.floor(Math.random() * (i + 1));
-            [arr[i], arr[j]] = [arr[j], arr[i]];
-        }
-        return arr;
-    }
-
-    const quiz = props.allQuiz.map((quiz, id) => {
-
-        const optionsArr = (quiz.incorrect_answers.concat(quiz.correct_answer))
-        // console.log(optionsArr)
-        shuffleArray(optionsArr)
-        // console.log(optionsArr)
-
-        return (<div key={id} className="text-clr-blue-text space-y-5">
-            <p className="text-lg font-karla font-bold">{quiz.question}</p>
-            <div className="flex flex-wrap gap-5">
-                {optionsArr.map((option, id) =>
-                    <button key={id} className="border border-clr-blue-btn rounded-md py-1 px-6 font-inter font-medium hover:bg-clr-choose hover:border hover:border-clr-choose">
-                        {option}</button>
-                )}
-            </div>
-            <hr className="bg-slate-500"></hr>
-        </div>)
-    })
-
-    // const questionsElements = props.allQuiz.map(quiz => (
-    //     <div className="text-blue-900 space-y-5">
-    //         <p className="text-lg font-bold">{quiz.question}</p>
-    //         <div className="flex flex-wrap gap-5">
-    //             {quiz.incorrect_answers.map(options =>
-    //                 <button className="border border-blue-700 rounded-md py-1 px-6 font-medium hover:bg-slate-300 hover:border hover:border-slate-300">
-    //                     {options}</button>
-    //             )}
-    //             <button className="border border-blue-700 rounded-md py-1 px-6 font-medium hover:bg-slate-300 hover:border hover:border-slate-300">
-    //                     {quiz.correct_answer}</button>
-    //         </div>
-    //         <hr className="bg-slate-500"></hr>
-    //     </div>
-    // ))
-
     return (
-        <div className="space-y-5 max-w-5xl mx-auto my-11 p-5">
-            {/* {JSON.stringify(allQuiz, null, 2)} */}
-            {quiz}
-            <div className="flex justify-center items-center">
-                <p className="font-bold text-lg text-clr-blue-text mr-4">You scored 4/5 correct answers</p>
-                <button
-                    onClick={props.startGame}
-                    className="bg-clr-blue-btn text-clr-white px-6 py-2 rounded-lg font-semibold hover:bg-blue-700">Check answers</button>
+        
+            <div className="text-clr-blue-text space-y-5">
+                <p className="text-lg font-karla font-bold">{props.question}</p>
+                <div className="flex flex-wrap gap-5">
+                    <button className="border border-clr-blue-btn rounded-md py-1 px-6 font-inter font-medium hover:bg-clr-choose hover:border hover:border-clr-choose">
+                    </button>
+                </div>
+                <hr className="bg-slate-500"></hr>
             </div>
-        </div>
+    
     )
 }
