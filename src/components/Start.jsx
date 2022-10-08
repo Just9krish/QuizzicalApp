778fe49787e2a4714d760a8777fd922c36458e60
just9krish <just9krish434@gamil.com>
2022-10-08 21:33:23 +0530

    improve style

--- src/components/Start.jsx
+++ src/components/Start.jsx
@@ -1,6 +1,6 @@
 export default function Start(props) {
     return (
-        <div className="animate-fade-in flex justify-center items-center h-screen flex-col gap-4 p-5 text-center dark:bg-dark-bg transition duration-500">
+        <div className="animate-fade-in flex justify-center items-center h-screen flex-col gap-4 p-5 text-center bg-transparent dark:bg-dark-bg transition duration-500">
             <h1 className="text-clr-blue-text font-bold text-3xl font-karla md:text-6xl dark:text-dark-head transition duration-500">Quizzical</h1>
             <p className="text-clr-blue-text font-inter md:text-xl dark:text-dark-para transition duration-500">Click on the start button to test your knowledge!</p>
             <button
